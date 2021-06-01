610837df5368f827442d1a325a542477fd4f4897
Mr_Chimick <mikechimick@gmail.com>
2021-06-01 00:06:00 -0700

    Initial commit

---
+++ HarmonyPatches.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+
+namespace PuppetPermissions
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        static HarmonyPatches() => new Harmony("PuppetPermissions").PatchAll();
+    }
+
+    [HarmonyPatch]
+    static class CustomizerChangePatch
+    {
+        static MethodBase TargetMethod() => AccessTools.Method("Puppeteer.Customizer:Change");
+
+        static bool Prefix() => false;
+    }
+}
